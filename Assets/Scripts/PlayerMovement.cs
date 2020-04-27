a4780de40465b7a82d44ddbffea2af3ee03c3cb4
DESKTOP-N3S839F\Mark <markabdilla26@gmail.com>
2020-04-27 17:02:38 +0200

    Pacman basic movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] float speed = 2f;
+
+    private void Update()
+    {
+        MoveRightAndLeft();
+        MoveUpAndDown();
+    }
+
+    private void MoveRightAndLeft()
+    {
+        float translation = Input.GetAxis("Horizontal") * speed;
+        translation *= Time.deltaTime;
+        transform.Translate(translation, 0, 0);
+    }
+
+    private void MoveUpAndDown()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        translation *= Time.deltaTime;
+        transform.Translate(0, translation, 0);
+    }
+}
